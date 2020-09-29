2ffa427db9a0eab9e9c31d7774c6e1ed328c80b5
karinarocha <38292745+karinarocha@users.noreply.github.com>
2020-09-28 21:48:51 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const inputs = document.querySelectorAll(".input");
+
+
+function addcl(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
+
+function remcl(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == ""){
+		parent.classList.remove("focus");
+	}
+}
+
+
+inputs.forEach(input => {
+	input.addEventListener("focus", addcl);
+	input.addEventListener("blur", remcl);
+});
+
+$(document).ready(function(){
+	$('.menu-btn').click(function(){
+	  $('.menu-mobile-itens').toggleClass('active');
+	});
+  });
+
+
+
